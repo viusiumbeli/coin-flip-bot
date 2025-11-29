20ecb38d65a5da90c445fc7b307df5c7423e74b4
Vladislav Iusiumbeli <v.iusiumbeli@finyard.com>
2025-11-29 16:32:01 +0200

    SimulationService improvements

--- src/main/kotlin/com/trading/coinflip/api/experiment/ExperimentComparisonResponse.kt
+++ src/main/kotlin/com/trading/coinflip/api/experiment/ExperimentComparisonResponse.kt
@@ -1,7 +1,5 @@
 package com.trading.coinflip.api.experiment
 
-import com.trading.coinflip.api.experiment.ExperimentSummaryResponse
-
 data class ExperimentComparisonResponse(
     val experiments: List<ExperimentSummaryResponse>,
     val metrics: List<ComparisonMetricResponse>,
